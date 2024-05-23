42a8df139069ae88dcd507990037a4272a3f57b7
Lucas F. Guanabara <lucasff.guanabara@gmail.com>
2024-05-23 20:53:01 -0300

    practicing arrays

---
+++ arrays/arrays.js
@@ -0,0 +1,7 @@
+var numbers = [1, 2, 3, 4, 5];
+// ----
+console.log(numbers[2]);
+var names = ['Goku', 'Vegeta', 'Trunks'];
+console.log(names[0]);
+var resultSoma = numbers[0] + numbers[3]; //5
+console.log(resultSoma);
---
+++ arrays/arrays.ts
@@ -0,0 +1,13 @@
+let numbers: number[] = [1, 2, 3, 4, 5];
+
+// ---
+
+console.log(numbers[2]);
+
+let names: string[] = [ 'Goku', 'Vegeta', 'Trunks'];
+
+console.log(names[0]);
+
+let resultSoma = numbers[0] + numbers[3]; //5
+
+console.log(resultSoma)
