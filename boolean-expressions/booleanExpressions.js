7c905079f7e4c26fe4e6791ade0ee03f233be77f
Lucas F. Guanabara <lucasff.guanabara@gmail.com>
2024-06-17 21:06:55 -0300

    bollean

---
+++ boolean-expressions/booleanExpressions.js
@@ -0,0 +1,13 @@
+var x = 6;
+var y = 10;
+/*let eMaiorQue = y > x;
+
+console.log(eMaiorQue);
+
+let eMenorQue = x < y;
+console.log(eMenorQue);*/
+/*
+let eMaiorOuIgual = x >= y; // 6 >= 10
+console.log(eMaiorOuIgual);*/
+var eMenorOuIgual = x <= y;
+console.log(eMenorOuIgual);
---
+++ boolean-expressions/booleanExpressions.ts
@@ -0,0 +1,14 @@
+let x = 6;
+let y = 10;
+
+let eMaiorQue = y > x;
+console.log(eMaiorQue); // Exibe true
+
+let eMenorQue = x < y;
+console.log(eMenorQue); // Exibe true
+
+let eMaiorOuIgual = x >= y; // 6 >= 10
+console.log(eMaiorOuIgual); // Exibe false
+
+let eMenorOuIgual = x <= y; // 6 <= 10
+console.log(eMenorOuIgual); // Exibe true
