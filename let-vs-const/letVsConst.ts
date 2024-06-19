e766407d8d9b29fa065cc0b56505c9cd1fc7c1a3
Lucas F. Guanabara <lucasff.guanabara@gmail.com>
2024-06-18 22:01:19 -0300

    letVconst

---
+++ let-vs-const/letVsConst.ts
@@ -0,0 +1,24 @@
+let num1 = 24;
+const num2 = 3;
+
+num1 = 26;
+
+num2 = 4;
+
+const pi = 3.141;
+
+let string1 = "Lucas";
+
+const string2 = "Guanabara";
+
+string2 = "Fernandes";
+
+string1 = "Brabo";
+
+
+let boolean1 = true;
+
+boolean1 = false;
+
+const boolean2 = false;
+boolean2 = true;
