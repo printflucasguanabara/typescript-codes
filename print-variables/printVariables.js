7a355381622c6dae35b4ca08a6237c70befe0199
Lucas F. Guanabara <lucasff.guanabara@gmail.com>
2024-06-25 22:59:25 -0300

    printVariables

---
+++ print-variables/printVariables.js
@@ -0,0 +1,5 @@
+var myName = "Lucas";
+var age = 21;
+console.log(age);
+// Meu nome é 'Lucas' e tenho '21' anos
+console.log("Meu nome é ".concat(myName, " e tenho ").concat(age, " anos "));
---
+++ print-variables/printVariables.ts
@@ -0,0 +1,8 @@
+let myName = "Lucas";
+let age = 21;
+
+console.log(age);
+
+// Meu nome é 'Lucas' e tenho '21' anos
+
+console.log(`Meu nome é ${myName} e tenho ${age} anos`);
